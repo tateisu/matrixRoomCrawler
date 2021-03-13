81000699499cc54e3390de271bc2a838675092b9
tateisu <tateisu@gmail.com>
2021-03-13 12:46:53 +0900

    update

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ cd /z/matrixRoomList
 cp matrixRoomCrawler/build/libs/matrixRoomCrawler-1.0-SNAPSHOT.jar ./matrixRoomCrawler.jar
 java -jar ./matrixRoomCrawler.jar
 cd web
+git add public/avatar/
 git commit -a -m "auto update"
 git push
